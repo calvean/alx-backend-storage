3857a9d4669c050d76a7bfa09e7a035ea79f73cc
calvean <garnetsharara@gmail.com>
2023-05-09 15:16:48 +0200

    fixes

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,11 +1,16 @@
 -- script that creates a stored procedure ComputeAverageScoreForUser
-
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT)
 BEGIN
-    DECLARE avg_score DECIMAL(10,2);
-    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
-    UPDATE users SET avg_score = avg_score WHERE id = user_id;
-END$$
+    DECLARE avg_score FLOAT;
+    SET avg_score = (
+    SELECT AVG(score)
+    FROM corrections
+    AS C WHERE C.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
+$$
 DELIMITER ;
 
