126a8c0a694471d015b1a218746243967e0c8a1a
calvean <garnetsharara@gmail.com>
2023-05-09 12:33:14 +0200

    fixes

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -4,8 +4,9 @@
 --        email, string (255 characters), never null and unique
 --        name, string (255 characters)
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
+    name VARCHAR(255),
+    PRIMARY KEY(id)
 );
 
