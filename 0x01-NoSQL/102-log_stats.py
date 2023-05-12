9476f016d176dea01f6f50043475feae483fab64
calvean <garnetsharara@gmail.com>
2023-05-13 00:55:27 +0200

    changes

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python3
 """ stats about Nginx logs stored in MongoDB """
+
 from pymongo import MongoClient
 
-
 if __name__ == "__main__":
-    client = MongoClient('mongodb://localhost:27017')
-    logs_col = client.logs.nginx
+    """ check all elements in collection """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
 
-    x = logs_col.count_documents({})
-    print(f"{x} logs")
+    print(f"{collection.estimated_document_count()} logs")
 
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = logs_col.count_documents({"method": method})
-        print(f"method {method}: {count}")
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        method_count = collection.count_documents({'method': method})
+        print(f"\tmethod {method}: {method_count}")
 
-    count = logs_col.count_documents({"method": "GET", "path": "/status"})
-    print(f"{count} status check")
+    check_method = collection.count_documents({
+        'method': 'GET', 'path': "/status"
+    })
+    print(f"{check_method} status check")
 
     print("IPs:")
-    pipeline = [
+    top_ips = collection.aggregate([
         {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
         {"$sort": {"count": -1}},
-        {"$limit": 10}
-    ]
-    for doc in logs_col.aggregate(pipeline):
-        print(f"    {doc['_id']}: {doc['count']}")
+        {"$limit": 10},
+        {"$project": {"_id": 0, "ip": "$_id", "count": 1}}])
+    for ip in top_ips:
+        print(f"\t{ip.get('ip')}: {ip.get('count')}")
