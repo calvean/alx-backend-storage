a54871a211c84ae55cab8204f01f9fa5f04f84b5
calvean <garnetsharara@gmail.com>
2023-05-13 01:05:42 +0200

    task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 """ String Redis """
+
+import uuid
+import redis
+
+class Cache:
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
