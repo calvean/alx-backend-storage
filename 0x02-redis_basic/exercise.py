7431a57eed7145bad0c686fa2fd3bd709ec1fdb5
calvean <garnetsharara@gmail.com>
2023-05-13 01:10:31 +0200

    task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,12 +4,23 @@
 import uuid
 import redis
 
+
 class Cache:
+    """ Functionality Class """
+
     def __init__(self):
+        """ Constructor """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store Cache
+        Args:
+            data: information to store
+        Return:
+            Key or number uuid
+        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
