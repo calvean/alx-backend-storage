205298a08dbb694e15acc2e39bf8299d68a1b232
calvean <garnetsharara@gmail.com>
2023-05-13 00:22:20 +0200

    fixes

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" Log stats """
+
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+collection = client['logs']['nginx']
+
+num_logs = collection.count_documents({})
+print(f"{num_logs} logs")
+
+http_methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+for method in http_methods:
+    count = collection.count_documents({'method': method})
+    print(f"\t{count} {method} requests")
+
+count = collection.count_documents({'method': 'GET', 'path': '/status'})
+print(f"{count} requests with method=GET and path=/status")
